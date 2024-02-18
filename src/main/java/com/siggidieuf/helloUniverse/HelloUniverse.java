135fbcfcf7d8b97f5658e5f0dfe495982ebe5f78
Mouhamadou Fall <mouhamadou2.fall@gmail.com>
2024-02-18 14:11:18 +0100

    Utiliser if et else

--- src/main/java/com/siggidieuf/helloUniverse/HelloUniverse.java
+++ src/main/java/com/siggidieuf/helloUniverse/HelloUniverse.java
@@ -2,32 +2,14 @@ package com.siggidieuf.helloUniverse;
 
 public class HelloUniverse {
     public static void main(String... args){
-        //int nbPlanetes = 8;
-        //System.out.println("Aux dernières nouvelles, le nombre total de planètes dans le système solaire est de : " + nbPlanetes);
-        //nbPlanetes++;
-        //System.out.println("Il y a quelques années cependant, elles étaient au nombre de : " + nbPlanetes);
-
-        // formattage de chaînes de caractères
-        int age = 29;
-        System.out.printf("Mon age est %d, Oui je suis jeune", age);
-
-        if (age > 30 )
-            System.out.println("Oui, je fais plus jeune.");
-
-        int ageduCapitaine = 48;
-        if (ageduCapitaine > age){
-            System.out.println("L'age du capitaine est: "+ageduCapitaine);
-            System.out.println("Bonjour mon capitaine.");
-        }
-
-        // TP Fil Rouge - Sujet : Affichage conditionnel - Première instruction conditionnelle
+        // TP Fil Rouge - Sujet : Utiliser if et else
         String phraseIntro = "En %d, les planètes du système solaire était au nombre de: %d.";
         int nbPlanetes = 0;
         int annee = 2016;
-        if (annee < 2006){
+        if (annee < 2006) {
             nbPlanetes = 9;
         }
-        if (annee >= 2006){
+        else {
             nbPlanetes = 8;
         }
         System.out.printf(phraseIntro, annee, nbPlanetes);
