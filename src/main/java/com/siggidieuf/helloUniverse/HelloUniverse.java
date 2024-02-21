5926bf264b4bc31551fb84bd6b94135c9f266910
Mouhamadou Fall <mouhamadou2.fall@gmail.com>
2024-02-21 10:03:45 +0100

    Proposition de solution: utilisation if, else if et else

--- src/main/java/com/siggidieuf/helloUniverse/HelloUniverse.java
+++ src/main/java/com/siggidieuf/helloUniverse/HelloUniverse.java
@@ -3,16 +3,42 @@ package com.siggidieuf.helloUniverse;
 public class HelloUniverse {
     public static void main(String... args){
         // TP Fil Rouge - Sujet : Utiliser if et else
+        // String phraseIntro = "En %d, les planètes du système solaire était au nombre de: %d.";
+//        int nbPlanetes = 0;
+//        int annee = 2016;
+//        if (annee < 2006) {
+//            nbPlanetes = 9;
+//        } else {
+//            nbPlanetes = 8;
+//        }
+//        System.out.printf(phraseIntro, annee, nbPlanetes);
+
+        // TP Fil Rouge - Sujet : Utiliser if, else if et else
+
+        String msgErreur = "Le programme ne peut pas fournir de résultat pour l'année %d";
+        String msg1700 = "L'année est inférieure à 1700 alors le nombre de planètes est de %d";
+        String msg1800 = "L'année est inférieure à 1800 alors le nombre de planètes est de %d";
         String phraseIntro = "En %d, les planètes du système solaire était au nombre de: %d.";
         int nbPlanetes = 0;
         int annee = 2016;
-        if (annee < 2006) {
-            nbPlanetes = 9;
+        if (annee >= 1600 && annee <= 2020){
+            if (annee < 1700) {
+                nbPlanetes = 7;
+                System.out.printf(msg1700, nbPlanetes);
+            } else if (annee < 1800 && annee >= 1700) {
+                nbPlanetes = 8;
+                System.out.printf(msg1800, nbPlanetes);
+            } else if (annee >= 1800 && annee <= 2006) {
+                nbPlanetes = 9;
+                System.out.printf(phraseIntro, annee, nbPlanetes);
+            } else {
+                nbPlanetes = 8;
+                System.out.printf(phraseIntro, annee, nbPlanetes);
+            }
+        } else {
+            System.out.printf(msgErreur, annee);
         }
-        else {
-            nbPlanetes = 8;
-        }
-        System.out.printf(phraseIntro, annee, nbPlanetes);
+
 
 
 
